de6e39f8dbd4e5444d6bbb122d9674af2c09184a
farazproject <muhammad.gulfaraz@live.com>
2025-06-19 14:03:53 +0500

    comments added

--- views/userView.js
+++ views/userView.js
@@ -1,15 +1,16 @@
-function renderUser(res, user) {
-  res.json({
-    status: 'success',
-    data: user
+function renderUser(res, user) { // Function to render user data in JSON format
+  res.json({   // Send JSON response
+    status: 'success', // Indicate success status
+    data: user // Include user data
   });
 }
 
-function renderError(res, statusCode, message) {
-  res.status(statusCode).json({
-    status: 'error',
-    message
+function renderError(res, statusCode, message) { // Function to render error responses
+  res.status(statusCode).json({     // Send error response
+    status: 'error',   // Indicate error status
+    message  // Provide error message
   });
 }
 
-module.exports = { renderUser, renderError }; 
+module.exports = { renderUser, renderError };  // Export functions for use in controllers
+// This module handles rendering user data and error responses in JSON format
